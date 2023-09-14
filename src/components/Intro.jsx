fbc6d4bf430861b4aef289644da49b2519128493
Miguel Lupani <mlupani2@gmail.com>
2023-09-13 23:44:10 -0300

    cambios ultimos gif0

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,12 @@
 import React, { useRef, useCallback } from 'react'
 import { useEffect } from 'react';
 
-const text = "Over the years, we humans have lived on Earth. This has been our home and refuge.. Over time, societies have been created in which laws have been put in place to have self-control for everyone.. But. will it be that humans will be prepared for whats coming..";
+const text = "Over the years, we humans have lived on Earth. This has been our home and refuge.. Over time, societies have been created in which laws have been put in place to have self-control for everyone.. but, will it be that humans will be prepared for what what's coming?";
 
 const Intro = (props) => {
     const textRef = useRef(null);
+    const [showButton, setShowButton] = React.useState(false);
+    const [textEnd, setTextEnd] = React.useState('');
 
 const handleStart = () => {
     props.setConfig({...props.config, startGame: true, intro: false})
@@ -12,30 +14,38 @@ const handleStart = () => {
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-const writeText = useCallback(
-    async () => {
-        setTimeout(async () => {
-            for (const char of text) {
-                await delay(char === '.' ? 800 : 50);
-                textRef.current && (textRef.current.innerHTML += char);
-            }
-        }, 1200);
-    }
-, [])
+const writeText = async () => {
+    setTimeout(async () => {
+        setShowButton(true);
+        for (const char of text) {
+            await delay(char === '.'  || char === ',' ? 800 : 50);
+            textRef.current && (textRef.current.innerHTML += char);
+            setTextEnd(prev => prev + char);
+        }
+    }, 1200);
+}
 
 useEffect(() => {
     writeText();
 }, []);
 
+useEffect(() => {
+    if (textEnd === text) {
+        setTimeout(() => {
+            handleStart();
+        }, 3000)
+    }
+}, [textEnd, handleStart])
+
   return (
-    <div style={{width: 1200, height: 600, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 2, color: 'white', backgroundColor: 'black'}}>
+    <div style={{width: 1200, height: 600, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 2, color: 'white', backgroundColor: 'black', backgroundImage: "url('https://media0.giphy.com/media/fo2h0DjceLyKlLxvvO/giphy.gif?cid=ecf05e47n26etaj4qnqu1f0ynf6x04phmafir5ld2v54ujcs&ep=v1_gifs_search&rid=giphy.gif&ct=g')", backgroundSize: 'cover'}}>
         <div style={{width: 500}}>
-            <p ref={textRef} style={{fontSize : '20px'}}></p>
+            <p ref={textRef} style={{fontSize : '22px', fontWeight: 'bold'}}></p>
         </div>
         <audio src="assets/introduccion.opus" autoPlay>
             Your browser does not support the <code>audio</code> element.
         </audio>
-        <button onClick={handleStart} style={{width: 200, height: 50, border: '1px solid gray', color: 'white', display:'flex', justifyContent: 'center', alignItems: 'center', fontSize: 18}} >
+        <button onClick={handleStart} style={{width: 200, height: 50, border: '1px solid gray', color: 'white', justifyContent: 'center', alignItems: 'center', fontSize: 18, display: showButton ? 'flex': 'none'}} >
             Skip
         </button>
     </div>
